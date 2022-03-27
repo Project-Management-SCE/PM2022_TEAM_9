0d295ca54a438c0f7b431b0b6db8175da601d3ae
liran121211@ac.sce.ac.il <liran121211@gmail.com>
2022-03-27 17:08:42 +0300

    #IGNORE#

--- src/main/java/ui/loans/HelloApplication.java
+++ src/main/java/ui/loans/HelloApplication.java
@@ -4,12 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import postgresql.PostgreSQL;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        PostgreSQL sql = PostgreSQL.getInstance();
+        sql.openConnection();
+        sql.closeConnection();
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
