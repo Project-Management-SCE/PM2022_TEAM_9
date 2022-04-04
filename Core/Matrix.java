3cddf4a0f88df56483fe1cdbe3275cc84eb39e3d
Liran.S <63954850+liran121211@users.noreply.github.com>
2022-04-04 14:22:13 +0300

    BSPM2022T9-76
    BSPM2022T9-76

--- Core/Matrix.java
+++ Core/Matrix.java
@@ -5,7 +5,12 @@ public class Matrix {
     private final int columns;
     private final double[][] matrix;
 
-
+    /**
+     * Default Matrix Constructor.
+     *
+     * @param n_rows    (Matrix number of rows).
+     * @param n_columns (Matrix number of columns).
+     */
     protected Matrix(int n_rows, int n_columns) {
         this.rows = Math.abs(n_rows);
         this.columns = Math.abs(n_columns);
@@ -17,6 +22,13 @@ public class Matrix {
             }
     }
 
+    /**
+     * Matrix Constructor.
+     *
+     * @param n_rows    (Matrix number of rows).
+     * @param n_columns (Matrix number of columns).
+     * @param B         (2D double array)
+     */
     protected Matrix(int n_rows, int n_columns, double[][] B) {
         this.rows = Math.abs(n_rows);
         this.columns = Math.abs(n_columns);
@@ -26,6 +38,13 @@ public class Matrix {
             System.arraycopy(B[i], 0, this.matrix[i], 0, this.columns);
     }
 
+    /**
+     * Vector Constructor.
+     *
+     * @param n_rows    (Matrix number of rows).
+     * @param n_columns (Matrix number of columns).
+     * @param V         (1D double array)
+     */
     protected Matrix(int n_rows, int n_columns, double[] V) {
         this.rows = Math.abs(n_rows);
         this.columns = Math.abs(n_columns);
@@ -43,6 +62,11 @@ public class Matrix {
         }
     }
 
+    /**
+     * Copy Constructor.
+     *
+     * @param B (Matrix object.)
+     */
     protected Matrix(Matrix B) {
         this.rows = B.rows;
         this.columns = B.columns;
@@ -54,29 +78,61 @@ public class Matrix {
         }
     }
 
-
+    /**
+     * Get number of rows of Matrix.
+     *
+     * @return (int) number of rows.
+     */
     public int getRows() {
         return rows;
     }
 
+    /**
+     * Get number of columns of Matrix.
+     *
+     * @return (int) number of columns.
+     */
     public int getColumns() {
         return columns;
     }
 
-    protected void setValue(int row, int column, double value) {
+    /**
+     * Set value into specific cell in the Matrix.
+     *
+     * @param row    (Matrix row number).
+     * @param column (Matrix column number).
+     * @param value  (double) value to insert.
+     */
+    protected void setValue(int row, int column, double value) throws IndexOutOfBoundsException {
         if (!(row > this.rows) && !(column > this.columns))
             this.matrix[row][column] = value;
+        else
+            throw new IndexOutOfBoundsException("row/column values are out of range.");
+
     }
 
-    protected double getValue(int row, int column) {
+    /**
+     * Retrieve value from specific cell.
+     *
+     * @param row    (Matrix row number).
+     * @param column (Matrix column number).
+     * @return (double) value of specific Matrix cell.
+     */
+    protected double getValue(int row, int column) throws IndexOutOfBoundsException {
         if (!(row > this.rows) && !(column > this.columns))
             return this.matrix[row][column];
-        return 0.0;
+        throw new IndexOutOfBoundsException("row/column values are out of range.");
     }
 
-    protected Matrix product(Matrix B) throws IndexOutOfBoundsException {
+    /**
+     * Multiply current Matrix with given Matrix (B).
+     *
+     * @param B (Matrix object).
+     * @return new Matrix after multiplication.
+     */
+    protected Matrix dot(Matrix B) throws IndexOutOfBoundsException {
         if (this.columns != B.rows)
-            throw new IndexOutOfBoundsException(String.format("Cannot Multiply (%s,%s) By (%s,%s)", this.rows, this.columns, B.rows, B.columns));
+            throw new IndexOutOfBoundsException(String.format("Cannot Product (%s,%s) By (%s,%s)", this.rows, this.columns, B.rows, B.columns));
 
         Matrix temp = new Matrix(this.rows, B.columns);
         double cell;
@@ -92,7 +148,13 @@ public class Matrix {
         return temp;
     }
 
-    protected Matrix product(double num) {
+    /**
+     * Multiply (num) value to each cell of the Matrix.
+     *
+     * @param num (double value)
+     * @return Matrix with updated values.
+     */
+    protected Matrix multiply(double num) {
         for (int i = 0; i < this.rows; i++) {
             for (int j = 0; j < this.columns; j++)
                 this.matrix[i][j] *= num;
@@ -100,7 +162,29 @@ public class Matrix {
         return this;
     }
 
-    protected Matrix add(Matrix B) {
+    /**
+     * Multiply each value to each cell of the Matrices.
+     *
+     * @param B (Matrix object)
+     * @return Matrix with updated values.
+     */
+    protected Matrix multiply(Matrix B) {
+        if (this.rows != B.rows || this.columns != B.columns)
+            throw new IndexOutOfBoundsException(String.format("Cannot Multiply (%s,%s) By (%s,%s)", this.rows, this.columns, B.rows, B.columns));
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.columns; j++)
+                this.matrix[i][j] *= B.matrix[i][j];
+        }
+        return this;
+    }
+
+    /**
+     * Add values from given Matrix (B) to the current Matrix.
+     *
+     * @param B (Matrix object).
+     * @return Modified Matrix after addition.
+     */
+    protected Matrix add(Matrix B) throws IndexOutOfBoundsException {
         if (this.rows != B.rows || this.columns != B.columns)
             throw new IndexOutOfBoundsException(String.format("Matrices has different dimensions (%s,%s) By (%s,%s)", this.rows, this.columns, B.rows, B.columns));
 
@@ -111,6 +195,12 @@ public class Matrix {
         return this;
     }
 
+    /**
+     * add (num) value to each cell of the Matrix.
+     *
+     * @param num (double value)
+     * @return Matrix with updated values.
+     */
     protected Matrix add(double num) {
         for (int i = 0; i < this.rows; i++) {
             for (int j = 0; j < this.columns; j++)
@@ -119,6 +209,11 @@ public class Matrix {
         return this;
     }
 
+    /**
+     * Transpose matrix from nxm to mxn.
+     *
+     * @return transposed Matrix.
+     */
     protected Matrix transpose() {
         Matrix temp = new Matrix(this.columns, this.rows);
         for (int i = 0; i < temp.rows; i++) {
@@ -128,7 +223,70 @@ public class Matrix {
         return temp;
     }
 
+    protected Matrix argmax(int axis) {
+        Matrix temp = new Matrix(1, this.columns);
+        if (axis == 0) {
+            for (int i = 0; i < this.getColumns(); i++) {
+                double max_val = this.getValue(0, i);
+                for (int j = 1; j < this.getRows(); j++)
+                    if (this.getValue(j, i) > max_val)
+                        max_val = this.getValue(j, i);
+                temp.setValue(0, i, max_val);
+            }
+        }
+        if (axis == 1) {
+            temp = temp.transpose();
+            for (int i = 0; i < this.getRows(); i++) {
+                double max_val = this.getValue(i, 0);
+                for (int j = 1; j < this.getColumns(); j++)
+                    if (this.getValue(i, j) > max_val)
+                        max_val = this.getValue(i, j);
+                temp.setValue(i, 0, max_val);
+            }
+            temp = temp.transpose();
+        }
+        return temp;
+    }
 
+    /**
+     * Return the log (e) value of each cell of the Matrix.
+     *
+     * @return Matrix with updated values.
+     */
+    protected Matrix log() {
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (this.matrix[i][j] <= 0.0) // Avoid log(0).
+                    this.matrix[i][j] = 1 - 1e-7;
+                else
+                    this.matrix[i][j] = Math.log(this.matrix[i][j]);
+            }
+        }
+        return this;
+    }
+
+    /**
+     * Return the mean value of the Matrix.
+     *
+     * @return (double value).
+     */
+    protected double mean() {
+        double mean = 0.0;
+        for (int i = 0; i < this.rows; i++)
+            for (int j = 0; j < columns; j++)
+                mean += this.matrix[i][j];
+        try {
+            return mean / (this.rows * this.columns);
+        } catch (ArithmeticException e) {
+            return 0.0;
+        }
+    }
+
+    /**
+     * Prints Matrix in 2D shape
+     *
+     * @return (Prettified Matrix String)
+     */
     @Override
     public String toString() {
         StringBuilder matrix_output = new StringBuilder();
@@ -143,6 +301,12 @@ public class Matrix {
         return matrix_output.toString();
     }
 
+    /**
+     * Compare values of current Matrix to the given one.
+     *
+     * @param obj (Matrix object)
+     * @return True if equals else false.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Matrix) { // Validate matrices size
@@ -161,8 +325,12 @@ public class Matrix {
         return true;
     }
 
+    /**
+     * @param n_rows    (number of rows in matrix)
+     * @param n_columns (number of columns in matrix)
+     * @return (nxm Matrix filled with normal_distribution values)
+     */
     protected static Matrix random(int n_rows, int n_columns) {
-
         Matrix temp = new Matrix(Math.abs(n_rows), Math.abs(n_columns));
         for (int i = 0; i < temp.rows; i++) {
             for (int j = 0; j < temp.columns; j++)
@@ -170,5 +338,21 @@ public class Matrix {
         }
         return temp;
     }
+
+    public static Matrix clip(Matrix B, double min, double max) {
+        Matrix temp = new Matrix(B.rows, B.columns);
+        for (int i = 0; i < B.rows; i++) {
+            for (int j = 0; j < B.columns; j++) {
+                if (B.matrix[i][j] < min)
+                    temp.matrix[i][j] = min;
+                else if (B.matrix[i][j] > max)
+                    temp.matrix[i][j] = max;
+                else
+                    temp.matrix[i][j] = B.matrix[i][j];
+            }
+        }
+        return temp;
+    }
+
 }
 
