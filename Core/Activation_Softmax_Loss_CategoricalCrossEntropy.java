201f08f27763ef902388f5a190005686075679cc
Liran.S <63954850+liran121211@users.noreply.github.com>
2022-04-07 13:43:26 +0300

    BSPM2022T9-83
    BSPM2022T9-83

--- Core/Activation_Softmax_Loss_CategoricalCrossEntropy.java
+++ Core/Activation_Softmax_Loss_CategoricalCrossEntropy.java
@@ -1,4 +1,7 @@
 public class Activation_Softmax_Loss_CategoricalCrossEntropy {
+    //Softmax classifier - combined Softmax activation
+    //and cross-entropy loss for faster backward step
+
     private Activation activation;
     private Loss loss;
     private Matrix output;
@@ -13,30 +16,31 @@ public class Activation_Softmax_Loss_CategoricalCrossEntropy {
 
     private Matrix d_inputs;
 
-
+    //Creates activation and loss function objects
     protected Activation_Softmax_Loss_CategoricalCrossEntropy() {
         this.activation = new Activation_SoftMax();
         this.loss = new Loss_CategoricalCrossEntropy();
     }
 
-
+    //Forward pass
     public double forward(Matrix inputs, Matrix y_true) throws InvalidMatrixOperation, InvalidMatrixAxis, MatrixIndexesOutOfBounds, InvalidMatrixDimension {
-        this.activation.forward(inputs);
-        this.output = this.activation.output();
+        this.activation.forward(inputs); //Output layer's activation function
+        this.output = this.activation.output(); //Set the output
 
         Matrix forward_matrix = this.loss.forward(this.output, y_true);
-        return this.loss.calculate(forward_matrix);
+        return this.loss.calculate(forward_matrix); //Calculate and return loss value
     }
 
+    //Backward pass
     public void backward(Matrix d_values, Matrix y_true) throws InvalidMatrixAxis, MatrixIndexesOutOfBounds, InvalidMatrixDimension {
         if (y_true.shape() == 2)
             y_true = y_true.argmax(1);
 
         this.d_inputs = new Matrix(d_values);
 
-        for (int i = 0; i < d_inputs.getRows(); i++) {
+        for (int i = 0; i < d_inputs.getRows(); i++)
             d_inputs.setValue(i, (int) y_true.getValue(0, i), d_inputs.getValue(i, (int) y_true.getValue(0, i)) - 1);
-        }
+
         this.d_inputs.divide(d_values.getRows());
     }
 }
--- Core/LayerDense.java
+++ Core/LayerDense.java
@@ -1,43 +1,110 @@
-import com.github.sh0nk.matplotlib4j.PythonExecutionException;
-
-import java.io.IOException;
-import java.util.Arrays;
-
 public class LayerDense {
     private Matrix weights;
     private Matrix biases;
+
     private Matrix output;
     private Matrix inputs;
+
     private Matrix d_weights;
     private Matrix d_biases;
     private Matrix d_inputs;
 
-    protected LayerDense(int n_inputs, int n_neurons) throws InvalidMatrixDimension {
+    private Matrix weight_momentums;
+    private Matrix bias_momentums;
+
+    // Layer initialization
+    protected LayerDense(int n_inputs, int n_neurons) throws InvalidMatrixDimension, MatrixIndexesOutOfBounds {
         this.weights = Matrix.random(n_inputs, n_neurons).multiply(0.01);
         this.biases = new Matrix(1, n_neurons);
+
+        //        this.weights = new Dataset().getTest_data(n_inputs,n_neurons);
+        //        this.biases = new Dataset().getTest_classes(n_neurons);
     }
 
+    /**
+     * Backward Pass.
+     *
+     * @param d_values (Matrix object).
+     */
     protected void backward(Matrix d_values) throws MatrixIndexesOutOfBounds, InvalidMatrixDimension, InvalidMatrixAxis, InvalidMatrixOperation {
         this.d_weights = this.inputs.transpose().dot(d_values); //Gradients on parameters
         this.d_biases = d_values.sum(0); //Gradients on parameters
         this.d_inputs = d_values.dot(this.weights.transpose()); //Gradient on values
-
     }
 
     /**
-     * Calculate output values from inputs, weights and biases
+     * Forward Pass.
      *
      * @param inputs (Matrix object).
      */
     protected void forward(Matrix inputs) throws MatrixIndexesOutOfBounds, InvalidMatrixOperation, InvalidMatrixDimension {
-        this.inputs = new Matrix(inputs);
-        this.output = addBias(inputs.dot(this.weights), this.biases);
+        this.inputs = inputs; //Remember input values
+        this.output = addBias(inputs.dot(this.weights), this.biases); //Calculate output values from inputs, weights and biases
     }
 
+    public Matrix getWeights() {
+        return weights;
+    }
 
-    protected static Matrix addBias(Matrix B, Matrix V) throws IndexOutOfBoundsException, InvalidMatrixOperation {
+    public Matrix getBiases() {
+        return biases;
+    }
+
+    public Matrix getOutput() {
+        return output;
+    }
+
+    public Matrix getInputs() {
+        return inputs;
+    }
+
+    public Matrix get_d_weights() {
+        return d_weights;
+    }
+
+    public Matrix get_d_biases() {
+        return d_biases;
+    }
+
+    public Matrix get_d_inputs() {
+        return d_inputs;
+    }
+
+    public Matrix get_weight_momentums() {
+        return weight_momentums;
+    }
+
+    public Matrix get_bias_momentums() {
+        return bias_momentums;
+    }
+
+    public void setWeights(Matrix weights) {
+        this.weights = weights;
+    }
+
+    public void setBiases(Matrix biases) {
+        this.biases = biases;
+    }
+
+    public void set_d_weights(Matrix d_weights) {
+        this.d_weights = d_weights;
+    }
+
+    public void set_d_biases(Matrix d_biases) {
+        this.d_biases = d_biases;
+    }
+
+    public void set_weight_momentums(Matrix weight_momentum) {
+        this.weight_momentums = weight_momentum;
+    }
+
+    public void set_bias_momentums(Matrix bias_momentum) {
+        this.bias_momentums = bias_momentum;
+    }
+
+    protected static Matrix addBias(Matrix B, Matrix V) throws InvalidMatrixOperation, MatrixIndexesOutOfBounds {
         if (B.getColumns() != V.getColumns())
-            throw new IndexOutOfBoundsException(String.format("Matrices has different dimensions (%s,%s) By (%s,%s)", B.getRows(), B.getColumns(), V.getRows(), V.getColumns()));
+            throw new MatrixIndexesOutOfBounds(B, V);
         else
             B.add(V);
         return B;
@@ -51,63 +118,64 @@ public class LayerDense {
         Matrix y = new Dataset().getSpiral_classes();
 
         //Create Dense layer with 2 input features and 3 output values
-        LayerDense dense1 = new LayerDense(2, 3);
+        LayerDense dense1 = new LayerDense(2, 64);
 
         //Create ReLU activation (to be used with Dense layer):
         Activation activation1 = new Activation_ReLU();
 
         //Create second Dense layer with 3 input features (as we take output.
         //of previous layer here) and 3 output values (output values)
-        LayerDense dense2 = new LayerDense(3, 3);
+        LayerDense dense2 = new LayerDense(64, 3);
 
         // Create Softmax classifier's combined loss and activation
         Activation_Softmax_Loss_CategoricalCrossEntropy loss_activation = new Activation_Softmax_Loss_CategoricalCrossEntropy();
 
-        //Perform a forward pass of our training data through this layer
-        dense1.forward(X);
+        Optimizer_SGD optimizer = new Optimizer_SGD(1e-3,0.9);
 
-        //Perform a forward pass through activation function
-        //takes the output of first dense layer here
-        activation1.forward(dense1.output);
+        for (int epoch = 0; epoch < 10001; epoch++) {
 
-        //Perform a forward pass through second Dense layer
-        //takes outputs of activation function of first layer as inputs
-        dense2.forward(activation1.output());
+            //Perform a forward pass of our training data through this layer
+            dense1.forward(X);
 
-        //Perform a forward pass through the activation/loss function
-        //takes the output of second dense layer here and returns loss
-        double loss = loss_activation.forward(dense2.output, y);
+            //Perform a forward pass through activation function
+            //takes the output of first dense layer here
+            activation1.forward(dense1.output);
 
-        //Let's see output of the first few samples:
-        System.out.println(loss_activation.get_outputs());
+            //Perform a forward pass through second Dense layer
+            //takes outputs of activation function of first layer as inputs
+            dense2.forward(activation1.output());
 
-        //Print loss value
-        System.out.println("loss: " + loss);
+            //Perform a forward pass through the activation/loss function
+            //takes the output of second dense layer here and returns loss
+            double loss = loss_activation.forward(dense2.output, y);
 
-        //Calculate accuracy from output of activation2 and targets
-        //calculate values along first axis
-        Matrix predictions = loss_activation.get_outputs().argmax(1);
 
-        if (y.shape() == 2)
-            y = new Matrix(y.argmax(1));
-        double accuracy = Matrix.bitwiseCompare(predictions.transpose(), y).mean();
+            //Calculate accuracy from output of activation2 and targets
+            //calculate values along first axis
+            Matrix predictions = loss_activation.get_outputs().argmax(1);
 
-        //Print accuracy
-        System.out.println("accuracy: " + accuracy);
+            if (y.shape() == 2)
+                y = new Matrix(y.argmax(1));
 
-        //Backward pass
-        loss_activation.backward(loss_activation.get_outputs(), y);
-        dense2.backward(loss_activation.get_d_inputs());
-        activation1.backward(dense2.d_inputs);
-        dense1.backward(activation1.d_inputs());
+            double accuracy = Matrix.bitwiseCompare(predictions.transpose(), y).mean();
 
-        //Print gradients
-        System.out.println(dense1.d_weights);
-        System.out.println(dense1.d_biases);
-        System.out.println(dense2.d_weights);
-        System.out.println(dense2.d_biases);
+            if (epoch % 100 == 0) {
+                System.out.println("epoch: " + epoch + " acc: " + accuracy + " loss: " + loss + " lr: " + optimizer.get_current_learning_rate());
+            }
 
-        new Matrix(2,5);
+            //Backward pass
+            loss_activation.backward(loss_activation.get_outputs(), y);
+            dense2.backward(loss_activation.get_d_inputs());
+            activation1.backward(dense2.d_inputs); //<-----this has a problem right now!
+            dense1.backward(activation1.d_inputs());
+
+            optimizer.pre_update_params();
+            optimizer.update_params(dense1);
+            optimizer.update_params(dense2);
+            optimizer.post_update_params();
+
+
+        }
     }
 
 }
