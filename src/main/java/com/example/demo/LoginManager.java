bb9655c01a5f39c9f64050e54d8415146770542e
tamaram <tamaram@ac.sce.ac.il>
2022-04-09 12:41:28 +0300

    update with story BSPM2022T9-87 for basic user panel

--- src/main/java/com/example/demo/LoginManager.java
+++ src/main/java/com/example/demo/LoginManager.java
@@ -4,10 +4,11 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.logging.*;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
 import javafx.scene.control.TextField;
+
+import java.util.prefs.Preferences;
 
 import static com.example.demo.LoanApp.sql;
 
@@ -44,7 +45,7 @@ public class LoginManager {
      * show user panel
      */
     public void userPanel() {
-        System.out.println("go to user panel");
+
         userPanelManager userPanelManager = new userPanelManager(scene);
         userPanelManager.showUserPanel();
     }
@@ -63,8 +64,13 @@ public class LoginManager {
     public void authorize(TextField user, TextField pass) throws SQLException {
         String[][] x = sql.select("users", "username, password", String.format("username='%s',password='%s'", user.getText(), pass.getText()));
         PostgreSQL.prettyPrint(x);
-        if (x.length > 0)
+        if (x.length > 0) {
+            String[][] id = sql.select("users", "id", String.format("username='%s'", user.getText()));
+//            System.out.println(id[0][0]);
+            Preferences userPrefrences = Preferences.userRoot();
+            userPrefrences.put("username",id[0][0]);
             userPanel();
+        }
         else
             System.out.println("Error!!!!!!");
 
---
+++ src/main/java/com/example/demo/editProfileController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class editProfileController {
+
+    @FXML
+    private Button submit, back;
+
+    public void initManager(editProfileManager editProfileManager){
+        submit.setOnAction(event -> editProfileManager.editProfile());
+        back.setOnAction(event -> editProfileManager.goBack());
+    }
+
+}
