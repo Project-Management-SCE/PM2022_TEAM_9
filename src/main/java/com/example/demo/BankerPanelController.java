95ce6bc2c8367d8a118c41ce66c789d748e42702
bathen-avraham <73163794+bathen-avraham@users.noreply.github.com>
2022-05-13 13:52:26 +0300

    BSPM2022T9-127 BSPM2022T9-128 BSPM2022T9-129
    BSPM2022T9-127 BSPM2022T9-128 BSPM2022T9-129

--- src/main/java/com/example/demo/BankerPanelController.java
+++ src/main/java/com/example/demo/BankerPanelController.java
@@ -3,24 +3,42 @@ package com.example.demo;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Duration;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 
-public class BankerPanelController implements PropertyChangeListener {
+public class BankerPanelController implements PropertyChangeListener, Initializable {
     @FXML
     private MenuItem logoutButton, editProfileButton, todolistButton, aboutButton, modifyAccountButton, homeButton;
     @FXML
     private Label new_messages_quantity, message_icon;
+    @FXML
+    private TableView<LoansModel> loans_table;
+    @FXML
+    private TableColumn<LoansModel, Integer> reference_id_col;
+    @FXML
+    private TableColumn<LoansModel, String> client_name_col, request_date_col, status_col;
+    @FXML
+    private TableColumn<LoansModel, Double> loan_amount_col, remaining_amount_col;
+
 
     private Timeline msg_flasher;
     private Boolean isUserNotified = false;
+    protected final ObservableList<LoansModel> loans_list = FXCollections.observableArrayList();
 
     public void initManager(BankerPanelManager bankerPanelManager) throws SQLException {
         controlsConfiguration(bankerPanelManager);
@@ -51,6 +69,14 @@ public class BankerPanelController implements PropertyChangeListener {
             aboutManager.initializeScreen();
         });
 
+        // Sort Columns
+        remaining_amount_col.setSortType(TableColumn.SortType.DESCENDING);
+        client_name_col.setSortType(TableColumn.SortType.DESCENDING);
+        request_date_col.setSortType(TableColumn.SortType.DESCENDING);
+        loan_amount_col.setSortType(TableColumn.SortType.DESCENDING);
+        remaining_amount_col.setSortType(TableColumn.SortType.DESCENDING);
+        status_col.setSortType(TableColumn.SortType.DESCENDING);
+
 
         // new message icon blinker
         msg_flasher = new Timeline(
@@ -78,6 +104,40 @@ public class BankerPanelController implements PropertyChangeListener {
             new_messages_quantity.setText("(" + evt.getNewValue() + ")");
             message_icon.setVisible(true);
         }
+    }
+
+    /**
+     * Fill table with messages data
+     *
+     * @return ObservableList (MessageModel).
+     */
+    protected ObservableList<LoansModel> itemsToTable() {
+        try {
+            String[][] clients = LoanApp.sql.select("users", "id", "role=0");
+            for (String[] col : clients) {
+                String[][] loan_form_data = LoanApp.sql.select("loan_form_data", "reference_id", String.format("user_id=%s", col[0]));
+                String[][] loan_data = LoanApp.sql.select("loans", "*", String.format("user_id=%s", col[0]));
+                String[][] full_name = LoanApp.sql.select("clients", "first_name, last_name", String.format("user_id=%s", col[0]));
+                loans_list.add(new LoansModel(Integer.parseInt(loan_data[0][0]), loan_form_data[0][0], full_name[0][0] + " " + full_name[0][1], loan_data[0][2], Double.parseDouble(loan_data[0][3]), Double.parseDouble(loan_data[0][4]), loan_data[0][5]));
+            }
+            return loans_list;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        reference_id_col.setCellValueFactory(new PropertyValueFactory<>("reference_id"));
+        client_name_col.setCellValueFactory(new PropertyValueFactory<>("client_name"));
+        request_date_col.setCellValueFactory(new PropertyValueFactory<>("request_date"));
+        loan_amount_col.setCellValueFactory(new PropertyValueFactory<>("loan_amount"));
+        remaining_amount_col.setCellValueFactory(new PropertyValueFactory<>("remaining_amount"));
+        status_col.setCellValueFactory(new PropertyValueFactory<>("status"));
+        loans_table.setItems(itemsToTable());
     }
 
     public MenuItem getLogoutButton() {
@@ -118,5 +178,33 @@ public class BankerPanelController implements PropertyChangeListener {
 
     public Boolean getUserNotified() {
         return isUserNotified;
+    }
+
+    public TableView<LoansModel> getLoans_list() {
+        return loans_table;
+    }
+
+    public TableColumn<LoansModel, Integer> getLoan_id_column() {
+        return reference_id_col;
+    }
+
+    public TableColumn<LoansModel, String> getClient_name_column() {
+        return client_name_col;
+    }
+
+    public TableColumn<LoansModel, String> getRequest_date_column() {
+        return request_date_col;
+    }
+
+    public TableColumn<LoansModel, String> getStatus_column() {
+        return status_col;
+    }
+
+    public TableColumn<LoansModel, Double> getLoan_amount_column() {
+        return loan_amount_col;
+    }
+
+    public TableColumn<LoansModel, Double> getRemaining_amount_column() {
+        return remaining_amount_col;
     }
 }
