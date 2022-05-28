d82219d01458d32494fed1a7044a8d0b9c8fcc4c
Liran.S <63954850+liran121211@users.noreply.github.com>
2022-05-28 19:41:57 +0300

    Update LoginTest.java

--- src/main/test/LoginTest.java
+++ src/main/test/LoginTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class CarTest {
+public class LoginTest {
 
     String make = "Honda"; 
     String model = "Civic"; 
