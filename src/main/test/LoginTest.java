d4be879f8c6f6f8c468c440a19107426c11ba0e8
liran121211@ac.sce.ac.il <liran121211@gmail.com>
2022-05-28 19:51:02 +0300

    fix testing

--- src/main/test/LoginTest.java
+++ src/main/test/LoginTest.java
@@ -1,23 +1,50 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 
-public class LoginTest {
+import com.example.demo.PostgreSQL;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import javafx.stage.Stage;
+import org.testfx.framework.junit.ApplicationTest;
+import java.sql.SQLException;
 
-    String make = "Honda"; 
-    String model = "Civic"; 
-    Car testCar = new Car(make, model); 
-    
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class LoginTest extends ApplicationTest {
+    private static final PostgreSQL sql = PostgreSQL.getInstance();
+
+    @BeforeClass
+    public static void initSQL() {
+        sql.openConnection();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 
     @Test
-    public void testMake(){
-        System.out.println("Running Test 1!");
-        Assertions.assertEquals(make, "HONDAA","Expected vehicle make did not match actual vehicle make"); 
-    }
+    public void A1() {
+        try {
+            String[][] client_id, banker_id, manager_id;
+            // Client Login
+            client_id = sql.select("users", "id", String.format("username='%s', password='%s'", "yonatan", "1234"));
 
-    @Test 
-    public void testModel(){
-        System.out.println("Running Test 2!");
-        Assertions.assertEquals(model,testCar.getModel(),"Expected vehicle model did not match actual vehicle model"); 
+            // Banker Login
+            banker_id = sql.select("users", "id", String.format("username='%s', password='%s'", "liransm", "PM2022"));
+
+            // Manager Login
+            manager_id = sql.select("users", "id", String.format("username='%s', password='%s'", "tamaram", "PM2022"));
+
+            if (client_id[0].length > 0)
+                System.out.println("Client Found, ID: " + client_id[0][0]);
+
+            if (banker_id[0].length > 0)
+                System.out.println("Banker Found, ID: " + banker_id[0][0]);
+
+            if (manager_id[0].length > 0)
+                System.out.println("Manager Found, ID: " + manager_id[0][0]);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-    
 }
